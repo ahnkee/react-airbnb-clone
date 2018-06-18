cdc67f0429bdb0711bc03e12196747983a574695
Andrew Lee <andrew.lee@springboardauto.com>
2018-06-17 17:23:49 -0700

    set up basic express app

---
+++ packages/server/src/index.mjs
@@ -0,0 +1,23 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
+import config from './config.json';
+
+const app = express();
+app.server = http.createServer(app);
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.server.listen(process.env.PORT || config.port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on port ${app.server.address().port}`);
+});
+
+export default app;
