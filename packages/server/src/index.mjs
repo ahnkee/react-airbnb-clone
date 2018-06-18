1fc0d6ac39dab33ef2165336ef5aae8c2d806e84
Andrew Lee <andrew.lee@springboardauto.com>
2018-06-17 17:52:19 -0700

    connect to db / initialize sequelize

--- packages/server/src/index.mjs
+++ packages/server/src/index.mjs
@@ -3,7 +3,8 @@ import cors from 'cors';
 import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
-import config from './config.json';
+import connectToDb from './db';
+import config from './config';
 
 const app = express();
 app.server = http.createServer(app);
@@ -15,9 +16,18 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.server.listen(config.port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on port ${app.server.address().port}`);
+connectToDb((err, db) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database', err);
+    process.exitCode = 1;
+    return;
+  }
+
+  app.server.listen(config.port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Started on port ${app.server.address().port}`);
+  });
 });
 
 export default app;
